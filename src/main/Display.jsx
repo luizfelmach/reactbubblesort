af29b003273cc24e1bc11cb4be0cda7e7b843937
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-01-23 14:40:37 -0300

    Implement sort logic

--- src/main/Display.jsx
+++ src/main/Display.jsx
@@ -2,19 +2,66 @@ import { Component } from "react";
 import Axis from "../components/Axis";
 import "./Display.css";
 
+const initialState = {
+  values: [],
+  current: 0,
+  count: 0,
+};
+
+for (let i = 0; i < 15; i++) {
+  const value = Math.random() * (10 - 1 + 1) + 1;
+  initialState.values.push(value);
+}
+
 export class Display extends Component {
+  state = { ...initialState };
+
+  constructor(props) {
+    super(props);
+    this.sort = this.sort.bind(this);
+  }
+
+  sort() {
+    const { current, values } = this.state;
+    if (current < values.length - 1) {
+      const newValues = [...this.state.values];
+      const _ =
+        this.state.values[this.state.current + 1] <
+        this.state.values[this.state.current]
+          ? true
+          : false;
+      if (_) {
+        newValues[this.state.current + 1] =
+          this.state.values[this.state.current];
+        newValues[this.state.current] =
+          this.state.values[this.state.current + 1];
+      }
+      const count = this.state.count + 1;
+      const values = [...this.state.values];
+      const current = this.state.current + 1;
+      this.setState({ values: newValues, current, count });
+      setTimeout(this.sort, 500);
+    } else {
+      if (this.state.count > 0) {
+        this.setState({ ...this.state.values, count: 0, current: 0 });
+        setTimeout(this.sort, 100);
+      } else {
+        return;
+      }
+    }
+  }
+
   render() {
     let rows = [];
-    for (let i = 0; i < 20; i++) {
-      rows.push(
-        <Axis key={i} value={Math.floor(Math.random() * (10 - 1 + 1)) + 1} />
-      );
-    }
+    this.state.values.forEach((value, index) => {
+      rows.push(<Axis key={index} value={value} />);
+    });
+
     return (
       <div className="display">
         {rows}
         <div className="button">
-          <button>Sort</button>
+          <button onClick={this.sort}>SORT</button>
         </div>
       </div>
     );
